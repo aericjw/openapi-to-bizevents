2e4383e67b4f339f7b41bc807b657c21978689d5
aericjw <aericwalls26@gmail.com>
2024-12-05 01:03:39 -0700

    chore: removed unused imports

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-import { useState } from 'react';
 
 import { useCurrentTheme } from "@dynatrace/strato-components/core";
 import { Flex } from "@dynatrace/strato-components/layouts";
-import { UploadIcon } from '@dynatrace/strato-icons';
-import { Button } from '@dynatrace/strato-components/buttons';
-import { Paragraph } from "@dynatrace/strato-components";
 
 import { UploadButton } from "../components/UploadButton";
 
